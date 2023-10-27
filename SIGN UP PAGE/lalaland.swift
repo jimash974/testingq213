8c1cae4019547100b38111ed7bea3c4d2c141c6c
jimash974 <jeremynchristopher@gmail.com>
2023-10-27 16:43:46 +0700

    laland file

---
+++ SIGN UP PAGE/lalaland.swift
@@ -0,0 +1,22 @@
+//
+//  lalaland.swift
+//  SIGN UP PAGE
+//
+//  Created by Jeremy Christopher on 27/10/23.
+//
+
+import Foundation
+import UIKit
+
+class lalaland: UIView{
+    init(){
+        super.init(frame: .zero)
+        print("sadads")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
