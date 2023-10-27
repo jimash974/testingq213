6e864e1310db7b72579c7972225f87d8ef98daee
jimash974 <jeremynchristopher@gmail.com>
2023-10-27 16:42:44 +0700

    Testfile

---
+++ SIGN UP PAGE/test123.swift
@@ -0,0 +1,21 @@
+//
+//  test123.swift
+//  SIGN UP PAGE
+//
+//  Created by Jeremy Christopher on 27/10/23.
+//
+
+import Foundation
+import UIKit
+
+class test123: UIView{
+    
+    init(){
+        print("123")
+        super.init(frame: .zero)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
